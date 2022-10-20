98b964d3b4a3837f71eb2326cae017cc2a9c1407
enr0s <enr0s@WINBOX.localdomain>
2022-10-20 23:29:10 +0100

    Initial commit

---
+++ src/pqcheck.c
@@ -0,0 +1,194 @@
+/*--------------------------------------------------------------------
+pqChecker, shared library plug-in for OpenLDAP server / ppolicy overlay
+Checking of password quality.
+Copyright (C) 2014, Abdelhamid MEDDEB (abdelhamid@meddeb.net)  
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+---------------------------------------------------------------------*/
+
+#define _POSIX_SOURCE 1
+
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <portable.h>
+#include <syslog.h>
+#include <stdio.h>
+#include <libintl.h>
+#include <locale.h>
+
+#include <pqcheck.h>
+
+char sendPwdTag = '|';
+
+pp_status_t getStatus(char *pwd, char *forbiddens)
+{
+  pp_status_t rslt;
+  rslt.upperNbr = 0;
+  rslt.lowerNbr = 0;
+  rslt.digitNbr = 0;
+  rslt.specialNbr = 0;
+  rslt.forbiddenNbr = 0;
+	int pwdLen = strlen(pwd);
+	int frbdLen = strlen(forbiddens);
+  int i = 0;
+  int j = 0;
+	for (i = 0; i<pwdLen; i++) 
+  {
+    if (isupper(pwd[i]))
+    {
+      rslt.upperNbr++;
+    }else
+    if (islower(pwd[i]))
+    {
+      rslt.lowerNbr++;
+    }else
+    if (isdigit(pwd[i]))
+    {
+      rslt.digitNbr++;
+    }else
+    if (ispunct(pwd[i]))
+    {
+      rslt.specialNbr++;
+    }
+    for (j = 0; j<frbdLen; j++)
+    {
+      if (pwd[i] == forbiddens[j]) rslt.forbiddenNbr++;
+    }  
+  }
+  return rslt;
+}
+/*-------------------------------------------------------------
+* Return pp_params_t from char* params according the fmt format
+* When params is not operable all fields is set to -1
+---------------------------------------------------------------*/
+pp_params_t getParams(char *params, char *fmt)
+{
+  pp_params_t rslt;
+  rslt.sendPwd = false;
+  int rootLen = 8; //without send password tag
+  if ((strlen(params) > 1) && (params[1] == sendPwdTag)) //with send password tag
+  {
+    rootLen = 10;
+    rslt.sendPwd = params[0] == '1';
+  }
+  rslt.upperMin = -1;
+  rslt.lowerMin = -1;
+  rslt.digitMin = -1;
+  rslt.specialMin = -1;
+  rslt.forbiddens[0] = '\0';
+  if (isParamOperable(params))
+  {
+    int i =0;
+    // set mins characters
+    char minParam[3];
+    minParam[2] = '\0';
+    for (i=0; i<4; i++)
+    {
+      minParam[0] = params[i*2+(rootLen-8)];
+      minParam[1] = params[i*2+1+(rootLen-8)];
+      switch (fmt[i])
+      {
+        case 'U':
+          rslt.upperMin = atoi(minParam);
+          break;
+        case 'S':
+          rslt.specialMin = atoi(minParam);
+          break;  
+        case 'D':
+          rslt.digitMin = atoi(minParam);
+          break;  
+        case 'L':
+          rslt.lowerMin = atoi(minParam);
+          break;  
+      }
+    }
+    if ((rslt.upperMin > -1) &&
+        (rslt.lowerMin > -1) &&
+        (rslt.digitMin > -1) &&
+        (rslt.specialMin > -1))
+    {  
+      // set forbidden characters
+      i = rootLen;
+      int paramsLen = strlen(params);
+      while (i < PARAMS_DATA_MAXLEN)
+      {
+        if (i < paramsLen) rslt.forbiddens[i - rootLen] = params[i];
+        else rslt.forbiddens[i-rootLen] = '\0';
+        i++;
+      }
+    }
+  }   
+  return rslt;
+}
+
+// check if params is operable
+bool isParamOperable(const char * params)
+{
+  if (!params) return false;
+  int i = 0;
+  int rootLen = 8; //without send password tag
+  int paramsLen = strlen(params);
+  if ((paramsLen > 1) && (params[1] == sendPwdTag)) //with send password tag
+  {
+    rootLen = 10;
+  }
+  bool rslt = (paramsLen >= rootLen);
+  while ((i < rootLen) && (rslt == true))
+  {
+    if ((rootLen == 10) && (i == 1))
+    {
+      rslt = params[i] == sendPwdTag;
+    } else
+    {
+      rslt = isdigit(params[i]);
+    }
+    i++;
+  }
+  if (!rslt) syslog(LOG_ERR, _("The parameter [%s] does not operable."),params);
+  return rslt;
+}
+
+// check if format is operable
+bool isFormatOperable(const char * fmt)
+{
+  if (!fmt) return false;
+  int i = 0;
+  int count = 0;
+  int fmtLen = strlen(fmt);
+  bool rslt = (fmtLen == 4);
+  while ((i < 4) && (rslt))
+  {
+    switch (fmt[i])
+    {
+      case 'U':
+        count++;
+        break;
+      case 'S':
+        count = count + 10;
+        break;  
+      case 'D':
+        count = count + 20;
+        break;  
+      case 'L':
+        count = count + 30;
+        break;  
+      default : rslt = false;
+    }
+    i++;
+  }
+  if (rslt) rslt = (count == 61);
+  if (!rslt) syslog(LOG_ERR,_("The format [%s] does not operable."), fmt);
+  return rslt;
+}
---
+++ src/pqchecker.c
@@ -0,0 +1,83 @@
+/*--------------------------------------------------------------------
+pqChecker, shared library plug-in for OpenLDAP server / ppolicy overlay
+Checking of password quality.
+Copyright (C) 2014, Abdelhamid MEDDEB (abdelhamid@meddeb.net)  
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+---------------------------------------------------------------------*/
+
+#include <ctype.h>
+#include <syslog.h>
+#include <stdbool.h>
+#include <portable.h>
+#include <slap.h>
+#include <libintl.h>
+#include <locale.h>
+
+#include <pparamio.h>
+#include <comm.h>
+#include <pqcheck.h>
+#include <pqchecker.h>
+
+int 
+check_password(char *pPasswd, char **ppErrStr, Entry *e);
+
+int 
+check_password(char *pPasswd, char **ppErrStr, Entry *e)
+{
+  setlocale (LC_ALL, "");
+  bindtextdomain (PACKAGE, LOCALEDIR);
+  textdomain (PACKAGE);
+  openlog(PACKAGE, LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL4);
+  char *user = e->e_name.bv_val;
+  syslog(LOG_INFO, _("Checking password quality for %s."), user);
+  int rslt = LDAP_OPERATIONS_ERROR;
+  char strParams[PARAMS_DATA_MAXLEN+1]; 
+  if (readParams(strParams)) 
+  {
+    syslog(LOG_DEBUG, _("The quality parameters used: %s"),strParams);
+    pp_params_t params = getParams(strParams, PARAMS_STORAGE_FORMAT);
+    if ((params.upperMin > -1) && (params.lowerMin > -1) &&
+        (params.digitMin > -1) && (params.specialMin > -1))
+    {  
+      pp_status_t pwdStatus = getStatus(pPasswd, params.forbiddens);
+      if ((pwdStatus.upperNbr < params.upperMin) ||
+          (pwdStatus.lowerNbr < params.lowerMin) ||
+          (pwdStatus.digitNbr < params.digitMin) ||
+          (pwdStatus.specialNbr < params.specialMin) ||
+          (pwdStatus.forbiddenNbr > 0))
+      {
+        *ppErrStr = strdup(_("The password does not pass quality check."));
+        syslog(LOG_INFO, _("Password rejected."));
+      } else {
+        rslt = LDAP_SUCCESS;
+        syslog(LOG_INFO, _("Password accepted."));
+        if (params.sendPwd == true) {
+          syslog(LOG_DEBUG, _("Sending password to pqMessenger"));
+          sendPassword(pPasswd, user);
+        }
+      }
+    } else {
+      *ppErrStr = strdup(_("Unable to verify the password quality. Problem with parameters."));
+      rslt = LDAP_OPERATIONS_ERROR;
+      syslog(LOG_DEBUG, _("The passwords quality parameter is not operable."));
+    }
+
+  } else {
+    *ppErrStr = strdup(_("Unable to verify the quality of the password. Problem in parameters."));
+    rslt = LDAP_OPERATIONS_ERROR;
+    syslog(LOG_DEBUG, _("The passwords quality parameter is not found."));
+  }
+  closelog();
+	return (rslt);
+}
